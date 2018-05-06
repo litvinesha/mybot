47e2caae3fa0a87d6ec8fb3bd810b61bf4250ae6
Dasha <dashildochka@gmail.com>
2018-05-06 11:01:59 +0300

    jj

--- KnitSencei_bot/KnitSencei_bot/Program.cs
+++ KnitSencei_bot/KnitSencei_bot/Program.cs
@@ -24,11 +24,8 @@ namespace KnitSencei_bot
         static DataOfIdea idea = new DataOfIdea();
         static Keyboard keyboard = new Keyboard();
         static List<DataOfIdea> bla = idea.ReadFile("ideas.csv");
-<<<<<<< HEAD
         //static List<CalcData> cd = 
-=======
         static List<DataOfIdea> data = new List<DataOfIdea>();
->>>>>>> 3a537a0381c3125f90599364d615d34c9320fdfc
         static int last_message;
         //static int count = 0;
         static string select_product;
@@ -57,7 +54,7 @@ namespace KnitSencei_bot
             string name = $"{e.CallbackQuery.From.FirstName}{e.CallbackQuery.From.LastName}";
             Console.WriteLine($"{name} нажал(а) кнопку {buttonText}");
 
-             if (buttonText == "Указать данные")
+            if (buttonText == "Указать данные")
                 await Bot.SendTextMessageAsync(e.CallbackQuery.From.Id, "Укажи вид изделия, размер и данные о пряже");
             else if (buttonText == "Произвести расчет")
             {
@@ -89,74 +86,66 @@ namespace KnitSencei_bot
                     //await Bot.SendTextMessageAsync(e.Message.From.Id, answer);
                     break;
             }
-            
-            blaCount = bla.Count()-1;
-<<<<<<< HEAD
-            
 
+            blaCount = bla.Count() - 1;
             for (int i = 0; i <= product.Count() - 1; i++)
             {
                 if ((last_message + 1 == e.Message.MessageId) && (e.Message.Text.ToLower() == product[i].product.ToLower()))
                     select_product = product[i].product;
-            
+
             }
-        
+
             if (blaCount != 0)
-                {
+            {
 
                 if (select_product == bla[count_idea].Product.ToLower())
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, bla[count_idea].Name + "\n" + bla[0].Description).Wait();
+                    var FileUrl = string.Format(@"images//{0}", bla[count_idea].Photo);
+                    var stream = new FileStream(FileUrl, FileMode.Open);
+                    var fileToSend = new FileToSend(bla[count_idea].Photo, stream);
+                    await Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);
+                    count_idea++;
+                }
+                for (int i = 0; i <= product.Count() - 1; i++)
+                {
+                    if ((last_message + 1 == e.Message.MessageId) && (e.Message.Text.ToLower() == product[i].product.ToLower()))
+                    { select_product = product[i].product; }
+                }
+
+                if ((blaCount != 0) & (count_idea == 0))
+                {
+                    for (int i = 0; i <= blaCount; i++)
                     {
-                        Bot.SendTextMessageAsync(e.Message.Chat.Id, bla[count_idea].Name + "\n" + bla[0].Description).Wait();
-                        var FileUrl = string.Format(@"images//{0}", bla[count_idea].Photo);
+                        if (select_product == bla[i].Product.ToLower())
+                        {
+                            DataOfIdea tmp = new DataOfIdea();
+                            tmp.ID = bla[i].ID;
+                            tmp.Name = bla[i].Name;
+                            tmp.Product = bla[i].Product;
+                            tmp.Photo = bla[i].Photo;
+                            tmp.Description = bla[i].Description;
+                            data.Add(tmp);
+                        }
+                    }
+
+
+                    //if ((last_message + 1 == e.Message.MessageId) && (e.Message.Text.ToLower() != product[count].product.ToLower()))
+                    //    Bot.SendTextMessageAsync(e.Message.From.Id, "Otvali Vasya").Wait();
+                    if (count_idea == 5)
+                    { Bot.SendTextMessageAsync(e.Message.Chat.Id, "К сожалению, в данной категории больше нет идей! Для того, чтобы найти что-то другое нужно выбрать /ideas").Wait(); select_product = ""; data.Clear(); count_idea = 0; }
+                    else if (data.Count() != 0)
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, data[count_idea].Name + "\n" + data[count_idea].Description).Wait();
+                        var FileUrl = string.Format(@"images//{0}", data[count_idea].Photo);
                         var stream = new FileStream(FileUrl, FileMode.Open);
-                        var fileToSend = new FileToSend(bla[count_idea].Photo, stream);
+                        var fileToSend = new FileToSend(data[count_idea].Photo, stream);
                         await Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);
                         count_idea++;
                     }
-      
-=======
-            for (int i = 0; i <= product.Count() - 1; i++)
-            {
-                if ((last_message + 1 == e.Message.MessageId) && (e.Message.Text.ToLower() == product[i].product.ToLower()))
-                { select_product = product[i].product; }
-            }
-        
-            if ((blaCount != 0) & (count_idea == 0))
-            {
-                for (int i = 0; i <= blaCount; i++)
-                {
-                    if (select_product == bla[i].Product.ToLower())
-                    {
-                        DataOfIdea tmp = new DataOfIdea();
-                        tmp.ID = bla[i].ID;
-                        tmp.Name = bla[i].Name;
-                        tmp.Product = bla[i].Product;
-                        tmp.Photo = bla[i].Photo;
-                        tmp.Description = bla[i].Description;
-                        data.Add(tmp);
-                    }
->>>>>>> 3a537a0381c3125f90599364d615d34c9320fdfc
                 }
-
-<<<<<<< HEAD
-         
-            //if ((last_message + 1 == e.Message.MessageId) && (e.Message.Text.ToLower() != product[count].product.ToLower()))
-            //    Bot.SendTextMessageAsync(e.Message.From.Id, "Otvali Vasya").Wait();
-=======
-            if (count_idea == 5)
-            { Bot.SendTextMessageAsync(e.Message.Chat.Id, "К сожалению, в данной категории больше нет идей! Для того, чтобы найти что-то другое нужно выбрать /ideas").Wait(); select_product = ""; data.Clear(); count_idea = 0; }
-            else if (data.Count() != 0)
-            {
-                Bot.SendTextMessageAsync(e.Message.Chat.Id, data[count_idea].Name + "\n" + data[count_idea].Description).Wait();
-                var FileUrl = string.Format(@"images//{0}", data[count_idea].Photo);
-                var stream = new FileStream(FileUrl, FileMode.Open);
-                var fileToSend = new FileToSend(data[count_idea].Photo, stream);
-                await Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);
-                count_idea++;
             }
->>>>>>> 3a537a0381c3125f90599364d615d34c9320fdfc
         }
-
-
     }
 }
+
